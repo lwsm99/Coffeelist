00ddc8a756bf193cb49503a5ceaf713777f080a2
Lucas <lucasweissmann@gmx.de>
2022-08-14 17:23:32 +0200

    finish up stroke detection

--- detect_objects.py
+++ detect_objects.py
@@ -6,12 +6,27 @@ import cv2
 import pytesseract
 import numpy as np
 import re
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+import matplotlib
+from matplotlib import pyplot as plt
+import tkinter
+matplotlib.use('TkAgg')
 
 
 ### GLOBAL VARIABLES ###
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 global names
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+configs = config_util.get_configs_from_pipeline_file('Tensorflow/workspace/models/my_ssd_mobnet/pipeline.config')
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore('Tensorflow/workspace/models/my_ssd_mobnet/ckpt-7').expect_partial()
+category_index = label_map_util.create_category_index_from_labelmap('Tensorflow/workspace/annotations/label_map.pbtxt')
 
 
 ### METHODS ###
@@ -89,8 +104,50 @@ def detect_names(image: str):
 
 
 # Detect single strokes & blocks of 5 and assign labels
-def detect_strokes():
-    print("Counting Strokes...")
+def detect_strokes(image: str):
+    img = cv2.imread(image)
+    image_np = np.array(img)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections = detect_fn(input_tensor)
+
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image_np_with_detections,
+        detections['detection_boxes'],
+        detections['detection_classes'] + label_id_offset,
+        detections['detection_scores'],
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=5,
+        min_score_thresh=.8,
+        agnostic_mode=False)
+
+    # Show Image for testing purposes
+    image_np_with_detections = cv2.resize(image_np_with_detections, (500, 666))
+    cv2.imshow('Result', image_np_with_detections)
+    cv2.waitKey(0)
+
+    # plt.imshow(image_np_with_detections)
+    # plt.show()
+
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
 
 
 # Assign bounding boxes to detected name individually using coordinates
