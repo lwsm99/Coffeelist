e058a357b5fd6d93855e2fdade9c119e3f6325c4
Lucas <lucasweissmann@gmx.de>
2022-08-18 15:54:36 +0200

    export data into excel sheet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Main Script which initializes the app
 from detect_objects import *
+from export_data import *
 import numpy as np
 
 # Initialize app
@@ -9,5 +10,6 @@ if __name__ == '__main__':
     objects = detect_strokes(img)
     names = detect_names(img)
     names = get_count_for_name(objects, names)
+    export_data(names)
 
     print(np.matrix(names))
